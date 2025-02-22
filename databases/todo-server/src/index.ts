7d7b28d2404178d701ff9058442b87fb72a8ff5e
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-22 14:24:03 +0900

    フロントエンドからの読み込みのためクロスドメインを開放（本番環境ではしない事）

--- databases/todo-server/src/index.ts
+++ databases/todo-server/src/index.ts
@@ -1,6 +1,5 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { createConnection } from "mysql2";
-import type { TypeTodo } from "./TypeTodo";
 import { read } from "./read";
 const app: express.Express = express();
 const port = 3000;
@@ -8,9 +7,19 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+// クロスドメインを開放（本番環境ではしない事）
+app.use((_request: Request, response: Response, next: NextFunction) => {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
+
 const connection = createConnection({
   host: "LOCALHOST",
-  user: "ROOT",
+  user: "USER",
   password: "PASSWORD",
   database: "DATABASENAME",
 });
