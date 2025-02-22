877dd271a5b5a6cf22373c65c413bad8e742c6c1
ShingoHosoda <mo0nrabb1t17882@gmail.com>
2025-02-22 14:25:35 +0900

    APIの読み込みテスト成功

--- databases/todo-front/src/main.ts
+++ databases/todo-front/src/main.ts
@@ -1,8 +1,30 @@
 import "./style.css";
+type TypeTodo = {
+  id: number;
+  title: string;
+  dueDate: Date;
+  completed: boolean;
+};
+
 const main = () => {
   const loadButton = document.querySelector(".load-button");
   if (!(loadButton instanceof HTMLButtonElement)) {
     return;
   }
+
+  const log = document.querySelector(".log");
+  if (!(log instanceof HTMLElement)) {
+    return;
+  }
+
+  loadButton.addEventListener("click", () => {
+    const load = async () => {
+      const data = await fetch("http://localhost:3000/read");
+      const todos: TypeTodo[] = await data.json();
+      console.log(todos);
+      log.innerHTML = JSON.stringify(todos, null, " ");
+    };
+    load(); // APIを読み込んで表示します。
+  });
 };
 window.addEventListener("load", main);
